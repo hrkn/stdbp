ebfc50543d5312246c052652a0c383d2273dd449
Haruki Nakayama <haruki.clef@gmail.com>
2014-02-03 03:50:50 +0900

    Applied formatter

--- sample/mutex/recursive_mutex.cpp
+++ sample/mutex/recursive_mutex.cpp
@@ -17,23 +17,33 @@ unsigned shared_resource = 0;
 std11::recursive_mutex rm;
 std11::hash<std11::thread::id> hash;
 
-bool recurser(int n, int origin) {
+bool recurser(int n, int origin)
+{
 	bool ret = true;
 	int level = origin - n + 1;
 
 	std11::lock_guard<std11::recursive_mutex> l(rm);
-	printf("Th[%08x] %s acquired Level %d lock\n"
-		, hash(std11::this_thread::get_id()), bracket_indent[level - 1], level);
+	printf("Th[%08x] %s acquired Level %d lock\n",
+			hash(std11::this_thread::get_id()), bracket_indent[level - 1],
+			level);
 
-	if (--n) { recurser(n, origin); }
-        else {
-		shared_resource += origin;
-		printf("Th[%08x] incremented %d. (shared_resource = %d)\n"
-			, hash(std11::this_thread::get_id()), origin, shared_resource);
-		if (shared_resource >= LIMIT) { ret = false; }
+	if (--n)
+	{
+		recurser(n, origin);
 	}
-	printf("Th[%08x] %s released Level %d lock\n"
-		, hash(std11::this_thread::get_id()), bracket_indent[level - 1], level);
+	else
+	{
+		shared_resource += origin;
+		printf("Th[%08x] incremented %d. (shared_resource = %d)\n",
+				hash(std11::this_thread::get_id()), origin, shared_resource);
+		if (shared_resource >= LIMIT)
+		{
+			ret = false;
+		}
+	}
+	printf("Th[%08x] %s released Level %d lock\n",
+			hash(std11::this_thread::get_id()), bracket_indent[level - 1],
+			level);
 
 	return ret;
 }
@@ -41,10 +51,11 @@ bool recurser(int n, int origin) {
 void worker()
 {
 	bool cont = true;
-	do {
+	do
+	{
 		int n = RANDOM(MIN_LEVEL, MAX_LEVEL);
 		printf("Th[%08x] calling recurse(%d) ...\n",
-			hash(std11::this_thread::get_id()), n);
+				hash(std11::this_thread::get_id()), n);
 		cont = recurser(n, n);
 		std11::this_thread::yield();
 	} while (cont);
@@ -53,20 +64,28 @@ void worker()
 #define THREAD_COUNT 4
 int main()
 {
-	srand((unsigned int)time(NULL));
+	srand((unsigned int) time(NULL));
 
-	for (unsigned i = 0; i < MAX_LEVEL; ++i) {
-		for (unsigned c = 0; c <= i; ++c) { bracket_indent[i][c] = '>'; }
+	for (unsigned i = 0; i < MAX_LEVEL; ++i)
+	{
+		for (unsigned c = 0; c <= i; ++c)
+		{
+			bracket_indent[i][c] = '>';
+		}
 		bracket_indent[i][i + 1] = '\0';
 	}
 
 	std11::thread th[THREAD_COUNT];
-	for (unsigned i = 0; i < THREAD_COUNT; ++i) {
+	for (unsigned i = 0; i < THREAD_COUNT; ++i)
+	{
 		std11::thread t(&worker);
 		th[i] = std11::move(t);
 	}
 
-	for (unsigned i = 0; i < THREAD_COUNT; ++i) { th[i].join(); }
+	for (unsigned i = 0; i < THREAD_COUNT; ++i)
+	{
+		th[i].join();
+	}
 
 	return 0;
 }
