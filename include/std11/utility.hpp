776e6edb57c1c54c16bf664b72111f1cc5d38fcf
Haruki Nakayama <haruki.clef@gmail.com>
2014-03-07 00:18:52 +0900

    Added partially specialized std::pair
    To avoid compilation error which says "forming reference
     to reference type T"
    
    refered:
    http://stackoverflow.com/questions/3769781/stdpair-of-references

--- include/std11/utility.hpp
+++ include/std11/utility.hpp
@@ -11,15 +11,43 @@
 #include <utility>
 #include "std11/_config.hpp"
 
+namespace std
+{
+// partially specialized pair to avoid "reference of reference" problem
+template<class T1, class T2>
+struct pair<T1&, T2&>
+{
+	typedef T1& first_type;
+	typedef T2& second_type;
+	first_type first;
+	second_type second;
+	pair(T1& t1, T2& t2) :
+			first(t1), second(t2)
+	{
+	}
+	template<class U1, class U2>
+	pair(pair<U1, U2> const& p) :
+			first(p.first), second(p.second)
+	{
+	}
+	template<class U1, class U2>
+	pair& operator=(const pair<U1, U2>& p)
+	{
+		first = p.first;
+		second = p.second;
+		return *this;
+	}
+};
+}
+
 namespace std11
 {
 
-#if STD11_CPP11_ENABLED
+#if STDBP_CPP11_ENABLED
 #define STD11_RV_REF(t)  t&&
 using std::move;
 #else
 #define STD11_RV_REF(t)  std11::RV<t> const
-#endif
 
 template<typename T>
 struct RV
@@ -47,6 +75,13 @@ STD11_RV_REF(T) move(T& t)
 	return tobe_moved;
 }
 
+using std::pair;
+
+// TODO redefine as which meet C++11 standard
+using std::make_pair;
+
 }  // namespace std11
+
+#endif /* STDBP_CPP11_ENABLED */
 
 #endif /* STDBP_STD11_UTILITY_HPP_ */
