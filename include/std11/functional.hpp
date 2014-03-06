31f7ea61e0ef867c69af7250c340836a6821511d
Haruki Nakayama <haruki.clef@gmail.com>
2014-03-07 00:26:30 +0900

    Imported C++03 defined functions

--- include/std11/functional.hpp
+++ include/std11/functional.hpp
@@ -762,6 +762,46 @@ struct hash<long double>
 	}
 };
 
+using std::plus;
+using std::minus;
+using std::multiplies;
+using std::divides;
+using std::modulus;
+using std::negate;
+using std::equal_to;
+using std::not_equal_to;
+using std::less;
+using std::less_equal;
+using std::greater;
+using std::greater_equal;
+using std::logical_and;
+using std::logical_or;
+using std::logical_not;
+using std::bit_and;
+using std::bit_or;
+using std::bit_xor;
+using std::unary_negate;
+using std::not1;
+using std::binary_negate;
+using std::not2;
+using std::unary_function;
+using std::binary_function;
+using std::pointer_to_unary_function;
+using std::pointer_to_binary_function;
+using std::ptr_fun;
+using std::mem_fun_t;
+using std::mem_fun1_t;
+using std::mem_fun_ref_t;
+using std::mem_fun1_ref_t;
+using std::const_mem_fun_t;
+using std::const_mem_fun1_t;
+using std::mem_fun;
+using std::mem_fun_ref;
+using std::binder1st;
+using std::bind1st;
+using std::binder2nd;
+using std::bind2nd;
+
 }  // namespace std11
 
 #endif /* STDBP_STD11_FUNCTIONAL_HPP_ */
